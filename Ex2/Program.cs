bf327b16dda1ebd7cb5476698e870498633588b9
Max <maksim101101@gmail.com>
2023-07-16 14:25:18 +0400

    Выполнил задание 2

---
+++ Ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+
+*/
+
+Console.Clear();
+
+int f(int x)
+{
+    int res = 0;
+    while (x > 0)
+    {
+        res = res + x % 10;
+        x /= 10;
+    }
+    return res;
+}
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(number));
+
